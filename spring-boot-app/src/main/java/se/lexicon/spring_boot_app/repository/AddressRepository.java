3fbbba50d89c34b48200d33d680874a602855e6a
ornella28 <angelnibaruta88@gmail.com>
2026-02-19 13:26:23 +0100

    created crudRepositories for the optional parts in Address

--- spring-boot-app/src/main/java/se/lexicon/spring_boot_app/repository/AddressRepository.java
+++ spring-boot-app/src/main/java/se/lexicon/spring_boot_app/repository/AddressRepository.java
@@ -1,6 +1,7 @@
 package se.lexicon.spring_boot_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import se.lexicon.spring_boot_app.entity.Address;
 
 import java.util.List;
@@ -8,4 +9,16 @@ import java.util.List;
 public interface AddressRepository extends JpaRepository<Address,Long> {
 
     List<Address> findByZipcode(String zipcode);
+
+
+    /*
+    * Optional part
+    * */
+    List<Address> findByCityIgnoreCase(String city);
+    List<Address> findByStreetContainingIgnoreCase(String street);
+    long countByZipcode(@Param("zip") String zipcode);
+    List<Address> findByZipcodeStartingWith(String zipcode);
+
+
+
 }
