6eafd63479e29952ba53f80167e55d3b95ed24bf
ornella28 <angelnibaruta88@gmail.com>
2026-02-19 13:02:38 +0100

    created crudRepositories for the optional parts in customer

--- spring-boot-app/src/main/java/se/lexicon/spring_boot_app/repository/CustomerRepository.java
+++ spring-boot-app/src/main/java/se/lexicon/spring_boot_app/repository/CustomerRepository.java
@@ -4,6 +4,7 @@ import org.hibernate.boot.models.JpaAnnotations;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.lexicon.spring_boot_app.entity.Customer;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +17,21 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByEmail(String email);
     List<Customer> findByLastNameIgnoreCase(String lastName);
     List<Customer> findByAddressCityIgnoreCase(String city);
+
+    /*
+    * Optional part
+    * */
+
+    List<Customer> findByEmailContainingIgnoreCase(String email);
+    List<Customer> findByCreatedAtAfter(Instant createdAt);
+    List<Customer> findByCreatedAtBetween(Instant start, Instant end);
+
+    long countByAddressCity(String city);
+    boolean existsByEmail(String email);
+
+
+
+
 
 
 
