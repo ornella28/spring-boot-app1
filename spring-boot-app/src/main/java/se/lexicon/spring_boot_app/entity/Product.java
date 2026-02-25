50a126da10ad8fe61a2ee0bb8dad10c3931792db
ornella28 <angelnibaruta88@gmail.com>
2026-02-25 10:44:33 +0100

    created entity product and its table

--- spring-boot-app/src/main/java/se/lexicon/spring_boot_app/entity/Product.java
+++ spring-boot-app/src/main/java/se/lexicon/spring_boot_app/entity/Product.java
@@ -1,7 +1,35 @@
 package se.lexicon.spring_boot_app.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)// we use Fetch type Lazy to avoid heavier loads
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
 
 }
