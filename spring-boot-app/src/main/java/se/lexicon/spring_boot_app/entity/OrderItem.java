89ab4a5b8fae88f22ef90abdfc20ccfb53a56a83
ornella28 <angelnibaruta88@gmail.com>
2026-02-26 14:31:07 +0100

    created a reference relationship between OrderItem and product

--- spring-boot-app/src/main/java/se/lexicon/spring_boot_app/entity/OrderItem.java
+++ spring-boot-app/src/main/java/se/lexicon/spring_boot_app/entity/OrderItem.java
@@ -31,6 +31,10 @@ public class OrderItem {
     @JoinColumn(name = "order_id", nullable = false)
     private Order  order;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
 
 
 
