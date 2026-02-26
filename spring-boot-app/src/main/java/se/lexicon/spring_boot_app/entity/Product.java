57a213e937bbd48252d023472f5b6b67cd8c7f84
ornella28 <angelnibaruta88@gmail.com>
2026-02-26 11:03:44 +0100

    created promotion class and its table

--- spring-boot-app/src/main/java/se/lexicon/spring_boot_app/entity/Product.java
+++ spring-boot-app/src/main/java/se/lexicon/spring_boot_app/entity/Product.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -29,6 +31,15 @@ public class Product {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)// we use Fetch type Lazy to avoid heavier loads
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
+
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "products_promotions",
+            joinColumns =  @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "promotion_id")
+    )
+    private Set<Promotion> promotions = new HashSet<>();
 
 
 }
---
+++ spring-boot-app/src/main/java/se/lexicon/spring_boot_app/entity/Promotion.java
@@ -0,0 +1,31 @@
+package se.lexicon.spring_boot_app.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "promotions")
+public class Promotion {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+}
