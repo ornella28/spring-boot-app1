ecfd11566f1d435078524910a0fcdcd7f59bdc6b
ornella28 <angelnibaruta88@gmail.com>
2026-02-27 10:26:50 +0100

    created a Promotion repository and implemented the required methods

---
+++ spring-boot-app/src/main/java/se/lexicon/spring_boot_app/repository/PromotionRepository.java
@@ -0,0 +1,31 @@
+package se.lexicon.spring_boot_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import se.lexicon.spring_boot_app.entity.Promotion;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+
+    // find promotions that are active on a given date
+
+    List<Promotion> findActiveOn(@Param("date") LocalDate date);
+
+    //Optional part
+
+    Optional<Promotion> findByCodeIgnoreCase(String code);
+
+    List<Promotion> findByStartDateAfter(LocalDate date);
+
+    List<Promotion> findByEndDateBefore(LocalDate date);
+
+    List<Promotion> findByEndDateIsNull();
+
+    //find promotions active today
+
+    List<Promotion> findActiveToday();
+
+}
