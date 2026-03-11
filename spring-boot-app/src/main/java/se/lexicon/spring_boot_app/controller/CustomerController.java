9049e260ba56c9639219351f47a700601024b694
ornella28 <angelnibaruta88@gmail.com>
2026-03-11 10:50:01 +0100

    created customer controller

--- spring-boot-app/src/main/java/se/lexicon/spring_boot_app/controller/CustomerController.java
+++ spring-boot-app/src/main/java/se/lexicon/spring_boot_app/controller/CustomerController.java
@@ -1,6 +1,42 @@
 package se.lexicon.spring_boot_app.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.lexicon.spring_boot_app.dto.request.CustomerRequest;
+import se.lexicon.spring_boot_app.dto.response.CustomerResponse;
+import se.lexicon.spring_boot_app.service.CustomerService;
+
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<CustomerResponse> register(@RequestBody @Valid CustomerRequest request){
+
+        CustomerResponse response = customerService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> findById(@PathVariable Long id){
+        CustomerResponse response = customerService.findById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomerResponse> update(@PathVariable Long id, @RequestBody @Valid CustomerRequest request){
+        CustomerResponse response = customerService.update(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+
 
 
 }
