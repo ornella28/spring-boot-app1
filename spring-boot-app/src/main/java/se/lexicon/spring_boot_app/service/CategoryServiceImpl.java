c5c90452ecfe5f2c29b2f3b3c5af7449cdcfe404
ornella28 <angelnibaruta88@gmail.com>
2026-03-12 09:57:31 +0100

    created categoryService and categoryServiceImpl first before categoryController

--- spring-boot-app/src/main/java/se/lexicon/spring_boot_app/service/CategoryServiceImpl.java
+++ spring-boot-app/src/main/java/se/lexicon/spring_boot_app/service/CategoryServiceImpl.java
@@ -1,6 +1,44 @@
 package se.lexicon.spring_boot_app.service;
 
-public class CategoryServiceImpl {
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import se.lexicon.spring_boot_app.dto.response.CategoryResponse;
+import se.lexicon.spring_boot_app.entity.Category;
+import se.lexicon.spring_boot_app.exception.DuplicateResourceException;
+import se.lexicon.spring_boot_app.repository.CategoryRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    @Transactional
+    public CategoryResponse create(String name) {
+
+        if (categoryRepository.existsByNameIgnoreCase(name)) {
+            throw new DuplicateResourceException("Category already exits: " +  name);
+        }
+
+        Category category = new Category();
+        category.setName(name);
+
+        Category saved =  categoryRepository.save(category);
+
+        return new CategoryResponse(saved.getId(), saved.getName());
+    }
+
+    @Override
+    public List<CategoryResponse> findAll() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(category -> new CategoryResponse(category.getId(), category.getName()))
+                .toList();
+    }
 
 
 }
