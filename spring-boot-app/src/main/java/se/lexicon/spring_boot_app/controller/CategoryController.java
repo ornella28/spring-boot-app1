7a9651e5c54e57e6ea23991723f9d9ffaf8f3d46
ornella28 <angelnibaruta88@gmail.com>
2026-03-12 10:10:29 +0100

    created categoryController

--- spring-boot-app/src/main/java/se/lexicon/spring_boot_app/controller/CategoryController.java
+++ spring-boot-app/src/main/java/se/lexicon/spring_boot_app/controller/CategoryController.java
@@ -1,13 +1,32 @@
 package se.lexicon.spring_boot_app.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.lexicon.spring_boot_app.dto.response.CategoryResponse;
+import se.lexicon.spring_boot_app.service.CategoryService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> create(@RequestParam String name) {
+        CategoryResponse categoryResponse = categoryService.create(name);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> findAll() {
+        List<CategoryResponse> categories = categoryService.findAll();
+        return ResponseEntity.ok(categories);
+    }
 
 
 }
